cffe441ce3ae8ec3bd2bc031142730eeb70bd3a7
qkrehf2 <64183602+qkrehf2@users.noreply.github.com>
2021-07-16 00:03:29 +0900

    CreateStuffAddition/IE아이템삭제

---
+++ kubejs/server_scripts/script.js
@@ -0,0 +1,33 @@
+// priority: 0
+
+settings.logAddedRecipes = true
+settings.logRemovedRecipes = true
+settings.logSkippedRecipes = false
+settings.logErroringRecipes = true
+
+console.info('Hello, World! (You will see this line every time server resources reload)')
+
+onEvent('recipes', event => {
+	// Change recipes here
+	event.remove({output: 'create_stuff_additions:blazing_pickaxe'})
+	event.remove({output: 'create_stuff_additions:blazing_shovel'})
+	event.remove({output: 'create_stuff_additions:blazing_axe'})
+	event.remove({output: 'create_stuff_additions:blazing_cleaver'})
+	event.remove({output: 'create_stuff_additions:rose_quartz_pickaxe'})
+	event.remove({output: 'create_stuff_additions:rose_quartz_axe'})
+	event.remove({output: 'create_stuff_additions:rose_quartz_shovel'})
+	event.remove({output: 'create_stuff_additions:rose_quartz_sword'})
+	event.remove({output: 'create_stuff_additions:shadow_steel_axe'})
+	event.remove({output: 'create_stuff_additions:shadow_steel_pickaxe'})
+	event.remove({output: 'create_stuff_additions:shadow_steel_shovel'})
+	event.remove({output: 'create_stuff_additions:shadow_steel_sword'})
+	event.remove({output: 'immersiveengineering:furnace_heater'})
+})
+
+onEvent('item.tags', event => {
+	// Get the #forge:cobblestone tag collection and add Diamond Ore to it
+	// event.get('forge:cobblestone').add('minecraft:diamond_ore')
+
+	// Get the #forge:cobblestone tag collection and remove Mossy Cobblestone from it
+	// event.get('forge:cobblestone').remove('minecraft:mossy_cobblestone')
+})
