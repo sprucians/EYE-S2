7eec76c20fc87b6e7c6fff6ad29884d803d005c2
qkrehf2 <64183602+qkrehf2@users.noreply.github.com>
2021-08-14 19:50:03 +0900

    asd

--- kubejs/startup_scripts/script.js
+++ kubejs/startup_scripts/script.js
@@ -10,10 +10,12 @@ onEvent('item.registry', event => {
 	event.create('incomplete_component').displayName('미완성된 기계 부품').texture('kubejs:item/incomplete_component')
 	event.create('cobbles').displayName('돌멩이').texture('kubejs:item/pebbles')
 	event.create('gravel_dust').displayName('자갈 가루').texture('kubejs:item/gravel_dust')
+	event.create('sand_dust').displayName('모래 가루').texture('kubejs:item/sand_dust')
 	event.create('grass_strip').displayName('풀잎 묶음').texture('kubejs:item/grass_leaf')
 	event.create('grass_string').displayName('풀잎 줄').texture('kubejs:item/grass_string')
-	event.create('cooked_worm').displayName('지렁이 구이').texture('kubejs:item/worm').food(food => {food.hunger(2);food.saturation(1);food.fastToEat(true);})
-	event.create('cooked_apple').displayName('구운 사과').texture('kubejs:item/apple').food(food => {food.hunger(5);food.saturation(0.7);food.fastToEat(true);})
+	event.create('cooked_worm').displayName('지렁이 구이').texture('kubejs:item/worm').food(food => {food.hunger(2);food.saturation(0.7);food.fastToEat(true);})
+	event.create('cooked_apple').displayName('구운 사과').texture('kubejs:item/apple').food(food => {food.hunger(5);food.saturation(0.5);food.fastToEat(true);})
+	event.create('world_piece').displayName('세계의 파편').texture('kubejs:item/piece')
 })
 
 onEvent('block.registry', event => {
